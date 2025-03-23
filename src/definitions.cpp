56ff81bcacc0485dbf4cbb0ec91135c9e2f35c0e
UsrBinLuna <zephyr1112@protonmail.com>
2025-03-23 10:41:23 -0300

    fonts and stuff

--- src/definitions.cpp
+++ src/definitions.cpp
@@ -1,4 +1,4 @@
 #include "definitions.h"
 
 Chip8::Chip8()
-    : pc(0x200), sp(0), i_reg(0), dt(0), st(0), waitingKey(0) {display.fill(false); keypad.fill(false);}
+    : pc(0x200), sp(0), i_reg(0), dt(0), st(0), waitingKey(0) {ram.fill(0x00); display.fill(false); keypad.fill(false);}
